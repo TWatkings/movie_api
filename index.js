591e6dc395c6d25a63f9e17d2d818061224f06b5
TWatkings <106554653+TWatkings@users.noreply.github.com>
2022-09-01 18:35:26 -0400

    added environment variables

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static('public'));
 app.use(morgan('common'));
